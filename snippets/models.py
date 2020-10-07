4ab455e022f63ed8b9cf3aedd5432b1cb56b9e0b
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-10-07 16:53:50 +0600

    Added LEXERS Added LANGUAGE_CHOICES Added STYLE_CHOICES Added BaseModel Added Snippet model Added save method Added User model

---
+++ snippets/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_all_lexers, get_lexer_by_name
+from pygments.styles import get_all_styles
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
+
+
+class BaseModel(models.Model):
+    objects = models.Manager()
+    DoesNotExist = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class Snippet(BaseModel):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE, default='')
+    highlighted = models.TextField(default='')
+
+    def save(self, *args, **kwargs):
+        lexer = get_lexer_by_name(self.language)
+        linenos = self.linenos and 'table' or False
+        options = self.title and {'title': self.title} or {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
+
+
+class User(BaseModel):
+    name = models.CharField(max_length=20, blank=True, default='admin')
+    surname = models.CharField(max_length=30, blank=True, default='admin')
