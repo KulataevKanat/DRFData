ff9df44f7ddb1ffd2bbd8f9102fc36648ca01db5
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-10-08 23:57:18 +0600

    during

---
+++ employment/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EmploymentConfig(AppConfig):
+    name = 'employment'
---
+++ employment/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    objects = models.Manager()
+    DoesNotExist = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class Employment(BaseModel):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length='30', blank='true', default='employment')
+
+    user = models.ForeignKey('models.Users', related_name='employment', on_delete=models.SET_DEFAULT, default='')
+
+
+class Users(BaseModel):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length='20', blank='true', default='user_name')
+    surname = models.CharField(max_length='20', blank='true', default='user_surname')
+    age = models.SmallIntegerField(max_length='3', blank='true', default='user_age')
