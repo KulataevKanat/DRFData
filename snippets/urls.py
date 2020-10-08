9ba5657f2fad634f9b9a8c375f23cab2789dd76c
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-10-09 00:49:48 +0600

    new router registrations

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,6 +5,8 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('snippets', views.SnippetViewSet)
+router.register('admins', views.AdminViewSet)
+router.register('users', views.UserViewSet)
 
 urlpatterns = [url('', include(router.urls)),
                url('api-auth/', include('rest_framework.urls', namespace='rest_framework'))]
