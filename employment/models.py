8ee1bfef1291c965192cd2b805b6ef11454a12ac
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-10-09 21:50:36 +0600

    added Meta (verbose_name_plural)

--- employment/models.py
+++ employment/models.py
@@ -11,13 +11,16 @@ class BaseModel(models.Model):
 
 class Employment(BaseModel):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length='30', blank='true', default='employment')
+    name = models.CharField(max_length=30, blank='true', default='employment')
 
-    user = models.ForeignKey('models.Users', related_name='employment', on_delete=models.SET_DEFAULT, default='')
+    # user = models.ForeignKey('models.Users', related_name='employment', on_delete=models.SET_DEFAULT, default='')
 
 
 class Users(BaseModel):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length='20', blank='true', default='user_name')
-    surname = models.CharField(max_length='20', blank='true', default='user_surname')
-    age = models.SmallIntegerField(max_length='3', blank='true', default='user_age')
+    name = models.CharField(max_length=20, blank='true', default='user_name')
+    surname = models.CharField(max_length=20, blank='true', default='user_surname')
+    age = models.SmallIntegerField(blank='true', default='user_age')
+
+    class Meta:
+        verbose_name_plural = "user"
