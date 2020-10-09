7e60edacad211c0ed82177d2c1db75b72c6d5cf3
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-10-09 21:55:40 +0600

    registered views at the urls

---
+++ employment/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+
+from employment import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('employments', views.EmploymentViewSet)
+router.register('users', views.UsersViewSet)
+
+urlpatterns = [
+    url('', include(router.urls)),
+    url('api-auth/', include('rest_framework.urls', namespace='rest_employment'))
+]
