732fd91ecab0b216f9069b427a42296c957ac452
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-10-09 21:50:52 +0600

    added http methods

--- employment/views.py
+++ employment/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from employment.models import Employment, Users
+from employment.serializers import EmploymentSerializers, UserSerializer
+
+
+class EmploymentViewSet(viewsets.ModelViewSet):
+    queryset = Employment.objects.using('employment').all()
+    serializer_class = EmploymentSerializers
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.using('employment').all()
+    serializer_class = UserSerializer
