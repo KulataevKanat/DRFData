4d4c891ecbd29af4fb9ac5e08c6818b80334c66d
KulataevKanat <RaketaEnot4466@gmail.com>
2020-11-07 05:02:00 +0600

    new routing registered

--- employment/urls.py
+++ employment/urls.py
@@ -4,11 +4,12 @@ from employment import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+router.register('create_employments', views.CreateEmploymentViewSet)
+router.register('findByBarcode', views.FindEmploymentByBarcode, basename='findByBarcode')
 router.register('employments', views.EmploymentViewSet)
 router.register('users', views.UsersViewSet)
 
 urlpatterns = [
     url('', include(router.urls)),
-    url('employments/findByBarcode/(?P<barcode>[a-z0-9]+)/', views.FindEmploymentByBarcode.as_view()),
     url('api-auth/', include('rest_framework.urls', namespace='rest_employment'))
 ]
