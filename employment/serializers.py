606c9d7d50a6d02a6edd05e32926b0ab7629f148
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-11-07 02:30:35 +0600

    changed employment fields to '__all__'

--- employment/serializers.py
+++ employment/serializers.py
@@ -3,13 +3,15 @@ from rest_framework import serializers
 from employment.models import Employment, Users
 
 
-class EmploymentSerializers(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Employment
-        fields = ('url', 'id', 'created', 'name')
-
-
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Users
         fields = ('url', 'id', 'created', 'name', 'surname', 'age')
+
+
+class EmploymentSerializers(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Employment
+        fields = '__all__'
