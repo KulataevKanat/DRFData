69d837c587536b1efb35e8fe49becc1db09d3f94
KulataevKanat <RaketaEnot4466@gmail.com>
2020-11-07 05:00:49 +0600

    added create employment

--- employment/serializers.py
+++ employment/serializers.py
@@ -9,6 +9,12 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'id', 'created', 'name', 'surname', 'age')
 
 
+class CreateEmploymentSerializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Employment
+        fields = ('url', 'id', 'created', 'name', 'user', 'barcode')
+
+
 class EmploymentSerializers(serializers.HyperlinkedModelSerializer):
     user = UserSerializer()
 
