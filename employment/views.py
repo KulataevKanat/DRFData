525982a4b391567b7d5a8b8bb21f6ae8c37b07c5
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-11-07 02:32:58 +0600

    added BarcodeFind class

--- employment/views.py
+++ employment/views.py
@@ -1,3 +1,4 @@
+from rest_framework import generics
 from rest_framework import viewsets
 
 from employment.models import Employment, Users
@@ -5,6 +6,12 @@ from employment.serializers import EmploymentSerializers, UserSerializer
 
 
 class EmploymentViewSet(viewsets.ModelViewSet):
+    queryset = Employment.objects.using('employment').all()
+    serializer_class = EmploymentSerializers
+
+
+class FindEmploymentByBarcode(generics.RetrieveAPIView):
+    lookup_field = 'barcode'
     queryset = Employment.objects.using('employment').all()
     serializer_class = EmploymentSerializers
 
